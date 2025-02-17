0e4103cc3a24bc7150dc8ba27dab84b8c2291054
kraaddys <137292231+kraaddys@users.noreply.github.com>
2025-02-17 14:45:21 +0200

    Add files via upload

---
+++ lab01/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+echo "Привет, мир!";
+echo "Hello, World with echo!";
+print "Hello, World with print!";
+*/
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+echo $days . " " . $message . "<br>";
+echo "$days $message <br>";
